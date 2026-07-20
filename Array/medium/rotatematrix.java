902c511bb569019c0e3580e697f16b7cc504fc32
Rutuja Khairnar <skhairnar.rutuja@gmail.com>
2026-07-20 23:55:04 +0530

    Solve Rotate Matrix problem

---
+++ Array/medium/rotatematrix.java
@@ -0,0 +1,26 @@
+public class rotatematrix {
+    public static void main(String[] args)
+    {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+
+        int n = matrix.length;
+        int[][] rotated = new int[n][n];
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                rotated[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+
+        for(int[] row : rotated)
+        {
+            for(int num : row)
+            {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
